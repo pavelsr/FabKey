43de7bca7eb3506181a2cbda805c16e6a3dba977
Pavel Serikov <pavel@fabmarkt.ru>
2017-11-23 16:18:59 +0300

    Changed DBUtil for multiple OR search

--- perl/DBUtil.pm
+++ perl/DBUtil.pm
@@ -5,6 +5,7 @@ package DBUtil;
 use DBI;
 use strict;
 use DBD::SQLite;
+# use Data::Dumper;
 
 =head1 Usage
 
@@ -16,36 +17,71 @@ use DBD::SQLite;
 sub new {
     my ($class, %args) = @_;
     $args{'dbh'} = DBI->connect($args{'dbi'},"","");
-    warn "New args: ".Dumper \%args;
     return bless \%args, $class;
 }
+
+sub construct_or_statement {
+  my ($self, %kv) = @_; # %kv - key-value hash
+  my $str = '('.(keys %kv)[0].'=\''.(values %kv)[0].'\')';
+  delete $kv{(keys %kv)[0]};
+  while (my ($key, $value) = each %kv) {
+    $str = join('OR', $str, '('.$key.'=\''.$value.'\')');
+  }
+  return $str;
+}
+
 
 
 =head1 search_user_in_db
 
-Search user in DB. Search by only one criteria (provided hash can have only one item)
+Search user in DB. Search by multiple criterias (OR logic for each)
 
-Return ONLY ONE RESULT (like https://metacpan.org/pod/DBI#fetchrow_hashref)
+Return ONLY ONE RESULT (like https://metacpan.org/pod/DBI#selectrow_hashref)
 
 Usage examples:
 
   search_user_in_db(telegram_username => 'serikoff');
   search_user_in_db(card_id => '12345678');
+  search_user_in_db(telegram_username => 'serikoff', card_id => '12345678');
 
 =cut
 
 
 sub search_user_in_db {
 	my ($self, %kv) = @_;  # $kv - key & value
-  my $search = {
-    field => (keys %kv)[0],
-    value => (values %kv)[0]
-  };
-  my $sth = $self->{dbh}->prepare("SELECT * FROM users WHERE ".$search->{field}." = ?")  or die $self->{dbh}->errstr;
-	$sth->execute($search->{value});
-	my $hash_ref = $sth->fetchrow_hashref;
-	return $hash_ref;
+  $self->{dbh}->selectrow_hashref('SELECT * FROM users WHERE '.$self->construct_or_statement(%kv));   # need to call from DBI::st instead of DBI::db
 }
+
+
+
+### OLD CODE
+
+
+# =head1 search_user_in_db
+#
+# Search user in DB. Search by only one criteria (provided hash can have only one item)
+#
+# Return ONLY ONE RESULT (like https://metacpan.org/pod/DBI#fetchrow_hashref)
+#
+# Usage examples:
+#
+#   search_user_in_db(telegram_username => 'serikoff');
+#   search_user_in_db(card_id => '12345678');
+#
+# =cut
+
+# sub search_user_in_db {
+# 	my ($self, %kv) = @_;  # $kv - key & value
+#   my $search = {
+#     field => (keys %kv)[0],
+#     value => (values %kv)[0]
+#   };
+#   my $sth = $self->{dbh}->prepare("SELECT * FROM users WHERE ".$search->{field}." = ?")  or die $self->{dbh}->errstr;
+# 	$sth->execute($search->{value});
+#    warn "STH: ".Dumper $sth;
+# 	my $hash_ref = $sth->fetchrow_hashref;
+# 	return $hash_ref;
+# }
 
 
 1;
