f0eadf3972e10fcb3e5fba2a7ef940a5aa0249d2
Pavel Serikov <pavel@fabmarkt.ru>
2017-11-27 12:01:36 +0300

    Fixed config.json problem and cpanfile

--- perl/bot_embedded.pl
+++ perl/bot_embedded.pl
@@ -15,8 +15,9 @@ use lib '.';
 use DBUtil;
 use Data::Dumper;
 
-my $config = plugin JSONConfig => {file => 'config.json'};
-
+if (-e 'config.json') {
+  my $config = plugin JSONConfig => {file => 'config.json'};
+}
 my $telegram_token = $ENV{FABKEY_BOT_TOKEN} || $config->{FABKEY_BOT_TOKEN};
 my $bot_name = '';
 my $api;
