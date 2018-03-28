89d53d375479121e1a2b47dc1de3f7aa46d51080
Pavel Serikov <pavelsr@cpan.org>
2018-03-28 13:24:38 +0300

    Backport: added Yancy and Makefile

---
+++ perl/yancy.pl
@@ -0,0 +1,60 @@
+use Mojolicious::Lite;
+use Mojo::SQLite;
+use Yancy;
+
+plugin Yancy => {
+    backend => 'sqlite:skud.db',
+    read_schema => 1,
+    collections => {
+        doors => {
+            properties => {
+                id => {
+                    type => 'integer',
+                    readOnly => 1,
+                }
+            }
+        },
+        entries => {
+            properties => {
+                id => {
+                    type => 'integer',
+                    readOnly => 1,
+                }
+            }
+        },
+        log => {
+            properties => {
+                id => {
+                    type => 'integer',
+                    readOnly => 1,
+                }
+            }
+        },
+        permissions => {
+            properties => {
+                id => {
+                    type => 'integer',
+                    readOnly => 1,
+                }
+            }
+        },
+        telegram_admission_requests => {
+            properties => {
+                id => {
+                    type => 'integer',
+                    readOnly => 1,
+                }
+            }
+        },
+        users => {
+            properties => {
+                id => {
+                    type => 'integer',
+                    readOnly => 1,
+                }
+            }
+        }
+    }
+};
+
+app->start;
